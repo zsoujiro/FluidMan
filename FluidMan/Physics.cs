2cf5274f4785b301ada790b08ac597a9a8327e2f
Nelson Sandoval Puente <nelsonsandovalpuente@tutanota.com>
2020-11-05 12:19:51 -0400

    Add project files.

---
+++ FluidMan/Physics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluidMan
+{
+    public class Physics
+    {
+        public List<double> Calc_Vel(List<double> v_0, double r_0, double r_i)
+        {
+            List<double> retval=new List<double>(v_0.Count);
+            for (int i = 0; i < v_0.Count; i++)
+            {
+                retval[i] = Math.Pow(r_0 / r_i, 2) * v_0[i];
+            }
+            return retval;
+        }
+    }
+}
